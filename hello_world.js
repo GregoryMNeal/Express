9c0c1247e6c5b9d87135f5c4fcf8c28c961e49df
Gregory Neal <gnealsr@gmail.com>
2017-11-04 14:32:21 -0500

    Hello World

---
+++ hello_world.js
@@ -0,0 +1,20 @@
+/*  Hello World
+
+Functional Requirements:
+
+1.  Make an express program that will display "Hello, world!" at the root URL: /
+
+*/
+
+var express = require('express'); // used to make an Express app
+var app = express();
+
+// get method for root URL:/
+app.get('/', fuction request, response) {
+  response.send('Hello World');
+});
+
+// Listen for requests
+app.listen(8000, function() {
+  console.log('* Listening on port 8000 *')
+});
